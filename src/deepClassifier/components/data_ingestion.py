bbaae809d1adcd44a35e78883a538b2cd118d684
Junior Eshetu <76874506+jjininu@users.noreply.github.com>
2022-09-16 03:04:17 -0400

    update entity

---
+++ src/deepClassifier/components/data_ingestion.py
@@ -0,0 +1,50 @@
+
+
+import os
+from deepClassifier.entiry import config_entity
+from configs import *
+from deepClassifier.config.ConfigurationManager import *
+from deepClassifier.constants import *
+from deepClassifier.utils.common import read_yaml, create_directories
+
+
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
+    def download_file(self):
+        if not os.path.exists(self.config.local_data_file):
+            filename, headers = request.urlretrieve(
+                url = self.config.source_URL,
+                filename = self.config.local_data_file
+            )
+
+    def _get_updated_list_of_files(self, list_of_files):
+        return [f for f in list_of_files if f.endswith(".jpg") and ("Cat" in f or "Dog" in f)]
+
+    def _preprocess(self, zf: ZipFile, f: str, working_dir: str):
+        target_filepath = os.path.join(working_dir, f)
+        if not os.path.exists(target_filepath):
+            zf.extract(f, working_dir)
+        
+        if os.path.getsize(target_filepath) == 0:
+            os.remove(target_filepath)
+
+    def unzip_and_clean(self):
+        with ZipFile(file=self.config.local_data_file, mode="r") as zf:
+            list_of_files = zf.namelist()
+            updated_list_of_files = self._get_updated_list_of_files(list_of_files)
+            for f in updated_list_of_files:
+                self._preprocess(zf, f, self.config.unzip_dir)
+
+    def initiate_data_ingestion(self):
+        try:
+            config = ConfigurationManager()
+            data_ingestion_config = config.get_data_ingestion_config()
+            data_ingestion = DataIngestion(config=data_ingestion_config)
+            data_ingestion.download_file()
+            data_ingestion.unzip_and_clean()
+        except Exception as e:
+            raise e
+         
+  
