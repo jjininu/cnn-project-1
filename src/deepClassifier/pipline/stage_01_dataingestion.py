3faa7a6e8df639512ee183f5717600b63d8dda8c
jjininu <jjininu@gmail.com>
2022-09-17 14:17:51 -0400

    update pipeline

--- src/deepClassifier/pipline/stage_01_dataingestion.py
+++ src/deepClassifier/pipline/stage_01_dataingestion.py
@@ -1,2 +1,18 @@
 from deepClassifier.components.data_ingestion import *
+from deepClassifier.config.config import ConfigurationManager
 
+def main() :
+    try:
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.unzip_and_clean()
+    except Exception as e:
+        raise e
+
+    if __name__ == "__main__":
+        try:
+            main()
+        except Exception as e:
+            raise e
